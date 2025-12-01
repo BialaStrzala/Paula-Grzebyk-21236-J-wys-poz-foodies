fe2ef11be889ecb8d63e0fdb1ae0bc6ce63d4f57
honoryk <118126185+honoryk@users.noreply.github.com>
2025-12-01 11:19:12 +0100

    lab 8

--- app/meals/page.js
+++ app/meals/page.js
@@ -6,8 +6,10 @@ export default function Home() {
     return (
       <>
         <header className={classes.header}>
-          <h1>Delicious meals, created{' '}</h1>
-          <span className={classes.highlight}>by you</span>
+          <h1>
+            Delicious meals, created{' '}
+            <span className={classes.highlight}>by you</span>
+          </h1>
           <p>Choose your favourite recipe and cook it yourself. It is easy and fun!</p>
           <p className={classes.cta}>
             <Link href="/meals/share">Share Your Favourite Recipe</Link>
